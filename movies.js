e05220fdd851a09b5a22e2fc0a1480e82ac7a50e
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2022-11-15 21:57:37 +0000

    Second commit: pop up working but not ideal.Yet need to work on select bar to sort movies

--- movies.js
+++ movies.js
@@ -2,19 +2,18 @@ let movieData = {
     "The Darjeeling Limited": {
       plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
       cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
-      runtime: 151,
-      rating: 7.2,
       year: 2007,
+      rating: 7.2,
+      runtime: 151,
     },
     "The Royal Tenenbaums": {
       plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
-      rating: 7.6,
-      year: 2001,
       cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+      year: 2001,
+      rating: 7.6,
       runtime: 170,
     },
     "Fantastic Mr. Fox": {
-      year: 2009,
       plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
       cast: [
         "George Clooney",
@@ -22,56 +21,81 @@ let movieData = {
         "Bill Murray",
         "Jason Schwartzman",
       ],
-      runtime: 147,
+      year: 2009,
       rating: 7.9,
+      runtime: 147,
     },
     "The Grand Budapest Hotel": {
-      rating: 8.1,
-      runtime: 159,
-      year: 2014,
       plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
       cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+      year: 2014,
+      rating: 8.1,
+      runtime: 159,
     }
   };
 
 
+// let arraySorte = Object.entries(movieData)
+// console.log(arraySorte)
+// let select = document.getElementById("selector")  
+// select.addEventListener("change" , () => {
+//   console.log(select.options[select.selectedIndex].value)
+// })
 
 
-//RIGHT ONE!!!!! 
-// console.log(TheGBH)
+
+// RIGHT ONE!!!!! 
+
 let titles = Object.keys(movieData)
 // Display movies
 let container = document.querySelector(".moviesContainer")
 titles.forEach( key => {
   
+  createCard(key)
+})
+
+let moviesArray = document.querySelectorAll(".movieCard").length
+for (let i = 0; i < moviesArray; i++) {
+  document.querySelectorAll(".movieCard")[i].addEventListener ("click", (e) => {
+    let selectedTitle = e.currentTarget.getAttribute("name")
+    let popUp = document.getElementById(selectedTitle)
+    popUp.classList.toggle("hide")
+  })
+} 
+
+function createCard(key) {
   let movieCard = document.createElement("div")
   let card = document.createElement("div")
   let movieTitle = document.createElement("div")
   let pic = document.createElement("img")
-  pic.src = "/static/video.png" 
+  pic.src = "/static/video.png"
+  let popUp = document.createElement("div")
   
   container.append(movieCard)
-  movieCard.append(card)
+  movieCard.append(card, movieTitle, popUp)
   card.append(pic)
-  movieCard.append(movieTitle)
+  popUp.setAttribute("id" , key)
   movieTitle.innerHTML = key;
 
   movieCard.setAttribute("name" , key)
   movieCard.classList.add("movieCard", "borders")
   movieTitle.classList.add("movietitle")
+  popUp.classList.add("popup")
+  fillContentPopUp(key, popUp);
+}
 
-  
-})
-
-let moviesArray = document.querySelectorAll(".movieCard").length
-for (let i = 0; i < moviesArray; i++) {
-  document.querySelectorAll(".movieCard")[i].addEventListener ("mousedown", (e) => {
-    let title = e.currentTarget.getAttribute("name")
-    console.log(title)
-    let popUp = document.createElement("div")
-    let currentDiv = document.getElementsByName(title)[0]
-    currentDiv.append(popUp)
+function fillContentPopUp(title, selectedDiv) {
+  // this accesses the Object with key = titleMovie
+  let movieObject = movieData[title] 
+  // Loop through key/value pair of the selected movie object
+  let keys = Object.keys(movieObject)
+  keys.forEach(key => {
+    let main = document.createElement("h2")
+    main.innerHTML = key
+    let paragraph = document.createElement("p")
+    paragraph.innerHTML = movieObject[key]
+    selectedDiv.append(main , paragraph)   
   })
-} 
-
-
+  
+  
+}
